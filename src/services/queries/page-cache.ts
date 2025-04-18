e5feaf70dfed5d227696ce2628d80076a3ef5cf0
Christian Bunk <general@101.simplelogin.com>
2025-04-18 18:38:44 +0200

    cache static pages

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,15 @@
-export const getCachedPage = (route: string) => {};
+import { client } from '$services/redis';
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signout'];
+
+const pageCacheKey = (id: string) => `pagecache#${id}`;
+
+export const getCachedPage = (route: string) => {
+	if (!cacheRoutes.includes(route)) return null;
+	return client.get(pageCacheKey(route))
+};
+
+export const setCachedPage = (route: string, page: string) => {
+	if (!cacheRoutes.includes(route)) return null;
+	return client.set(pageCacheKey(route), page, { EX: 2})
+};

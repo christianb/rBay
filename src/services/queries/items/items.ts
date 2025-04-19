4ffb4e79c3b68c875722ade757c99aa2a76593ac
Christian Bunk <christian.bunk@nordsec.com>
2025-04-19 22:10:10 +0200

    get all items

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -7,17 +7,31 @@ import { deserialize } from '$services/queries/items/deserialize';
 
 export const getItem = async (id: string) => {
 	const item = await redis.hGetAll(itemsKey(id));
-	if (Object.keys(item).length === 0) {
-		return null
-	}
-	return deserialize(id, item)
+	return extractFromRedisResponse(item, id);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return redis.hGetAll(itemsKey(id));
+	});
+
+	const results = await Promise.all(commands);
+
+	return results.map((result, i) => {
+		extractFromRedisResponse(result, ids[i]);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
 	const serialized = serialize(attrs);
-	await  redis.hSet(itemsKey(id), serialized);
-	return id
+	await redis.hSet(itemsKey(id), serialized);
+	return id;
+};
+
+const extractFromRedisResponse = (item, id: string) => {
+	if (Object.keys(item).length === 0) {
+		return null;
+	}
+	return deserialize(id, item);
 };

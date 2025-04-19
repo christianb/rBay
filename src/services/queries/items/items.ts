56170bf7ceb158a6edb33205228bcce6c2103290
Christian Bunk <christian.bunk@nordsec.com>
2025-04-19 19:56:35 +0200

    save and get item

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,23 @@
 import type { CreateItemAttrs } from '$services/types';
+import { redis } from '$services/redis';
+import { serialize } from '$services/queries/items/serialize';
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
+import { deserialize } from '$services/queries/items/deserialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await redis.hgetall(itemsKey(id));
+	if (Object.keys(item).length === 0) {
+		return null
+	}
+	return deserialize(id, item)
+};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const id = genId();
+	const serialized = serialize(attrs);
+	await  redis.hset(itemsKey(id), serialized);
+	return id
+};

b43eb3ee55fbce50e09eab33c418f69ba4915008
Christian Bunk <christian.bunk@nordsec.com>
2025-04-21 12:53:20 +0200

    user HyperLogLog for tracking approx unique item views

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,7 +1,10 @@
 import { redis } from '$services/redis';
-import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
+	const inserted = await redis.pfAdd(itemsViewsKey(itemId), userId)
+	if (!inserted) return
+
 	return Promise.all([
 		redis.hIncrBy(itemsKey(itemId), 'views', 1),
 		redis.zIncrBy(itemsByViewsKey(), 1, itemId)

3de626a3e1eee3a431284544e1e29108ae3cf42f
Christian Bunk <christian.bunk@nordsec.com>
2025-04-21 17:12:37 +0200

    check conditions before storing new bid

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,11 +1,26 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { redis } from '$services/redis';
 import { DateTime } from 'luxon';
+import { getItem } from '$services/queries/items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
+	const item = await getItem(attrs.itemId);
+
+	if (!item) throw Error(`Item "${attrs.itemId}" not found`);
+	if (item.price >= attrs.amount) throw Error(`Bid too low`);
+	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) throw Error(`Bidding closed for item"`);
+
 	const serialized = serializeBidHistory(attrs.amount, attrs.createdAt.toMillis());
-	return redis.rPush(bidHistoryKey(attrs.itemId), serialized);
+
+	Promise.all([
+		redis.rPush(bidHistoryKey(attrs.itemId), serialized),
+		redis.hSet(itemsKey(item.id), {
+			bids: item.bids + 1,
+			price: attrs.amount,
+			highestBidUserId: attrs.userId,
+		}),
+	]);
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {

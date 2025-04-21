7e9bc77ea7860748494e034d55bd31a67bed08f9
Christian Bunk <christian.bunk@nordsec.com>
2025-04-21 16:52:16 +0200

    create and read bids

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,29 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
+import { bidHistoryKey } from '$services/keys';
+import { redis } from '$services/redis';
+import { DateTime } from 'luxon';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const serialized = serializeBidHistory(attrs.amount, attrs.createdAt.toMillis());
+	return redis.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	const startIndex = -1 * offset - count;
+	const endIndex = -1 - offset;
+
+	const range = await redis.lRange(bidHistoryKey(itemId), startIndex, endIndex);
+	return range.map(bid => deserializeBidHistory(bid));
+};
+
+const serializeBidHistory = (amount: number, createdAt: number) => {
+	return `${amount}:${createdAt}`;
+};
+
+const deserializeBidHistory = (stored: string) => {
+	const [amount, createdAt] = stored.split(':');
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt)),
+	};
 };

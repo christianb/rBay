5e03cf38e4ef2dd358dbc1d796a195d5ec80660e
Christian Bunk <christian.bunk@nordsec.com>
2025-04-21 17:58:52 +0200

    fix concurrency issue when placing bids by using transactions and watch

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -5,22 +5,27 @@ import { DateTime } from 'luxon';
 import { getItem } from '$services/queries/items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const item = await getItem(attrs.itemId);
+	return redis.executeIsolated(async (isolatedClient) => {
+		await isolatedClient.watch(itemsKey(attrs.itemId));
 
-	if (!item) throw Error(`Item "${attrs.itemId}" not found`);
-	if (item.price >= attrs.amount) throw Error(`Bid too low`);
-	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) throw Error(`Bidding closed for item"`);
+		const item = await getItem(attrs.itemId);
 
-	const serialized = serializeBidHistory(attrs.amount, attrs.createdAt.toMillis());
+		if (!item) throw Error(`Item "${attrs.itemId}" not found`);
+		if (item.price >= attrs.amount) throw Error(`Bid too low`);
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) throw Error(`Bidding closed for item"`);
 
-	Promise.all([
-		redis.rPush(bidHistoryKey(attrs.itemId), serialized),
-		redis.hSet(itemsKey(item.id), {
-			bids: item.bids + 1,
-			price: attrs.amount,
-			highestBidUserId: attrs.userId,
-		}),
-	]);
+		const serialized = serializeBidHistory(attrs.amount, attrs.createdAt.toMillis());
+
+		isolatedClient
+			.multi()
+			.rPush(bidHistoryKey(attrs.itemId), serialized)
+			.hSet(itemsKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId,
+			})
+			.exec();
+	});
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {

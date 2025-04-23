3c948a13d7d6be27fa2d08895d78a9893854c95c
Christian Bunk <christian.bunk@nordsec.com>
2025-04-23 14:52:35 +0200

    search all items by user

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -1,3 +1,7 @@
+import { redis } from '$services/redis';
+import { itemsIndexKey } from '$services/keys';
+import { deserialize } from '$services/queries/items/deserialize';
+
 interface QueryOpts {
 	page: number;
 	perPage: number;
@@ -5,4 +9,31 @@ interface QueryOpts {
 	direction: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+export const itemsByUser = async (userId: string, opts: QueryOpts) => {
+	const query = `@ownerId:{${userId}}`;
+	const sortCriteria = opts.sortBy && opts.direction && {
+		BY: opts.sortBy, DIRECTION: opts.direction,
+	};
+
+	const { total, documents } = await redis.ft.search(
+		itemsIndexKey(),
+		query,
+		{
+			ON: 'HASH',
+			SORTBY: sortCriteria,
+			LIMIT: {
+				from: opts.page * opts.perPage,
+				size: opts.perPage,
+			},
+		},
+	);
+
+	const items = documents.map((itemKey: string, serializedItem: any) => {
+		deserialize(itemKey.replace('items#', ''), serializedItem);
+	});
+
+	return {
+		totalPages: Math.ceil(total / opts.perPage),
+		items: items,
+	};
+};
